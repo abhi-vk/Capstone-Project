d899f3cdc906f5d9ea9080566b1cd7363ec03b05
abhivk <97803980+abhi-vk@users.noreply.github.com>
2024-11-20 17:35:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Register />} />
+          <Route path="/register" element={<Register />} />
          
           
         </Routes>

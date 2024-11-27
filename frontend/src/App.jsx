f287e4faa3d6e3a51231fce5861ca22887953092
abhi-vk <abhishekabhi4123@gmail.com>
2024-11-27 13:48:02 +0530

    added vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,28 @@
-import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Login, Register, Home, Products, AddressPage } from './pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Footer from './components/footer'; // Make sure to import your Footer component
 import { CartProvider } from "./context/cartContext";
-
-const isProduction = process.env.NODE_ENV === 'production'; // Check if in production
-
 function App() {
   return (
     <>
-      {isProduction ? (
-        <HashRouter>  {/* Use HashRouter for production */}
-          <div className="app-wrapper">
-            <CartProvider>
-              <div className="content-wrapper">
-                <Routes>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/" element={<Home />} />
-                  <Route path="/products" element={<Products />} />
-                  <Route path="/address" element={<AddressPage />} />
-                </Routes>
-              </div>
-            </CartProvider>
-            <Footer />
+      <BrowserRouter>
+        <div className="app-wrapper"> {/* Wrapper for the entire layout */}
+        <CartProvider>
+          <div className="content-wrapper"> {/* Wrapper for content and footer */}
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/address" element={<AddressPage />} />
+            </Routes>
           </div>
-        </HashRouter>
-      ) : (
-        <BrowserRouter>  {/* Use BrowserRouter for local development */}
-          <div className="app-wrapper">
-            <CartProvider>
-              <div className="content-wrapper">
-                <Routes>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/" element={<Home />} />
-                  <Route path="/products" element={<Products />} />
-                  <Route path="/address" element={<AddressPage />} />
-                </Routes>
-              </div>
-            </CartProvider>
-            <Footer />
-          </div>
-        </BrowserRouter>
-      )}
+          </CartProvider>
+          <Footer /> {/* Footer at the bottom */}
+        </div>
+      </BrowserRouter>
       <Toaster />
     </>
   );
